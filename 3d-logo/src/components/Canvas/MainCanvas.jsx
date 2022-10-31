1986906a415259a31bccf64231d98cdbe4214355
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-31 12:38:00 +0200

    Increase canvas size

--- 3d-logo/src/components/Canvas/MainCanvas.jsx
+++ 3d-logo/src/components/Canvas/MainCanvas.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import RLogo from '../Logos/RLogo/RLogo';
+import './canvas.scss';
 
 export default function MainCanvas() {
   return (
-    <Canvas style={{ background: "#999999" }}>
-        <RLogo />
-    </Canvas>
+    <div className='canvasWrapper'>
+      <Canvas style={{ background: "#999999" }}>
+          <RLogo />
+      </Canvas>
+    </div>
   )
 }
